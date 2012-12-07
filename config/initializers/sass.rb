1046092fdf263ab86b29e515ef3702e80de4d72c
Christopher Erin <chris.erin@gmail.com>
2012-12-07 16:53:21 -0600

    remove sass init

--- config/initializers/sass.rb
+++
@@ -1,6 +0,0 @@
-Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
-  load_paths << "#{Rails.root}/app/assets/stylesheets"
-  load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
-  load_paths << "#{Gem.loaded_specs['html5-rails'].full_gem_path}/stylesheets"
-  load_paths << "#{Gem.loaded_specs['bourbon'].full_gem_path}/lib/stylesheets"
-end
