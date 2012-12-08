67fd3d5864f9da87d73b67e7c04841f06d52dddb
Christopher Erin <chris.erin@gmail.com>
2012-12-08 12:15:43 -0600

    production? not production

--- app/scraper_steps/clear_cache.rb
+++ app/scraper_steps/clear_cache.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class ClearCache < ScraperStep
   def run()
-    if Rails.env.production
+    if Rails.env.production?
       open("http://www.bballnumbers.com/clear_cache")
     end
   end
