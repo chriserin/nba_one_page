c421a337c12632a617e10e254a4002a789d121b2
Christopher Erin <chris.erin@gmail.com>
2012-12-20 11:59:22 -0600

    keep alive worker; rebuild cache

--- app/scraper_steps/clear_cache.rb
+++ app/scraper_steps/clear_cache.rb
@@ -3,5 +3,13 @@ require 'open-uri'
 class ClearCache < ScraperStep
   def run()
     open("http://www.bballnumbers.com/clear_cache")
+    rebuild_cache
+  end
+
+  def rebuild_cache
+    open("http://www.bballnumbers.com/")
+    Nba::TEAMS.each do |team, info|
+      open(URI.escape "http://www.bballnumbers.com/#{info[:nickname]}")
+    end
   end
 end
--- workers/schedule_worker.rb
+++ workers/schedule_worker.rb
@@ -2,7 +2,7 @@
 
 class ScheduleWorker
 
-  def self.schedule
+  def self.schedule_scraper
 
     client = IronWorkerNG::Client.new
     code = IronWorkerNG::Code::Base.new(:workerfile => 'workers/scrape_boxscores.worker')
@@ -10,9 +10,28 @@ class ScheduleWorker
     schedule = client.schedules.create('ScrapeBoxscores', {:env => :production, :mongohq_url => ENV["MONGOHQ_URL"] || "mongodb://localhost:27017/main"}, {:start_at => Time.utc(Time.now.year, Time.now.month, Time.now.day + 1, 7), :run_every => 60*60*24 })
   end
 
-  def self.run
+  def self.schedule_keep_alive
+
+    client = IronWorkerNG::Client.new
+    code = IronWorkerNG::Code::Base.new(:workerfile => 'workers/keep_alive.worker')
+    client.codes.create(code)
+    schedule = client.schedules.create('KeepAlive', { :env => :production }, {:start_at => Time.now, :run_every => 60*60 })
+  end
+
+  def self.schedule_all
+    schedule_scraper
+    schedule_keep_alive
+  end
+
+  def self.run_scraper
     client = IronWorkerNG::Client.new
     code = IronWorkerNG::Code::Base.new(:workerfile => 'workers/scrape_boxscores.worker')
+    code.run
+  end
+
+  def self.run_keep_alive
+    client = IronWorkerNG::Client.new
+    code = IronWorkerNG::Code::Base.new(:workerfile => 'workers/keep_alive.worker')
     code.run
   end
 end
