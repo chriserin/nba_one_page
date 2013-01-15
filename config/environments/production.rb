45fb70483cd9652ce5ce05c2393a2048eef2e654
Christopher Erin <chris.erin@gmail.com>
2013-01-15 12:49:39 -0600

    default logging level to warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ NbaOnePage::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -31,7 +31,7 @@ NbaOnePage::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
--- config/initializers/compile_pages.rb
+++ config/initializers/compile_pages.rb
@@ -1,7 +1,6 @@
-require './workers/schedule_worker'
-
 if Rails.env.production?
   Rails.application.config.after_initialize do
+    require './workers/schedule_worker'
     ScheduleWorker.schedule_rebuild_cache
   end
 end
