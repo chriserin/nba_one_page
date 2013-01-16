0ab2f7320edec92b339fa7ebd049031d4dce84a8
Christopher Erin <chris.erin@gmail.com>
2013-01-15 19:07:49 -0600

    only schedule rebuild_cache if ARGV is blank

--- config/initializers/compile_pages.rb
+++ config/initializers/compile_pages.rb
@@ -1,7 +1,9 @@
 if Rails.env.production?
   Rails.application.config.after_initialize do
     p "args after initialize #{ARGV}"
-    #require './workers/schedule_worker'
-    #ScheduleWorker.schedule_rebuild_cache
+    if ARGV.blank?
+      require './workers/schedule_worker'
+      ScheduleWorker.schedule_rebuild_cache
+    end
   end
 end
