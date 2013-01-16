d93b0ead49a806f8156de5f4daa72c41addef059
Christopher Erin <chris.erin@gmail.com>
2013-01-16 13:43:41 -0600

    make rebuild cache conditional

--- app/scraper_steps/scrape_boxscores.rb
+++ app/scraper_steps/scrape_boxscores.rb
@@ -1,22 +1,22 @@
-
-
 class ScrapeBoxscores
 
-  def self.scrape(game_date=DateTime.now - 1)
+  def self.scrape(game_date=DateTime.now - 1, rebuild=true)
     scoreboard_scraper = NbaScoreboardScraper.new nil
     urls = scoreboard_scraper.scrape_scoreboard(game_date)
 
     boxscore_scraper = NbaBoxscoreScraper.new(NbaBoxscoreConverter.new)
     boxscore_scraper.run(urls)
 
-    cache_clearer = ClearCache.new
-    cache_clearer.run()
+    if rebuild 
+      cache_clearer = ClearCache.new
+      cache_clearer.run()
+    end
   end
 
   def self.scrape_2013()
     GameLine.season2013.delete_all
     (DateTime.new(2012, 10, 29)..(DateTime.now - 1)).each do |date|
-      self.scrape(date)
+      self.scrape(date, false)
       sleep(2)
     end
   end
