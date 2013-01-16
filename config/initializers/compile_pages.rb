c30b50f87225c44608279fc57e5c273cf04461fc
Christopher Erin <chris.erin@gmail.com>
2013-01-15 18:11:16 -0600

    debug info to find caller

--- config/initializers/compile_pages.rb
+++ config/initializers/compile_pages.rb
@@ -1,6 +1,7 @@
 if Rails.env.production?
   Rails.application.config.after_initialize do
-    require './workers/schedule_worker'
-    ScheduleWorker.schedule_rebuild_cache
+    p caller
+    #require './workers/schedule_worker'
+    #ScheduleWorker.schedule_rebuild_cache
   end
 end
