69a9c1da0cf7ad7364e85cba4e5748c7b49368c2
Christopher Erin <chris.erin@gmail.com>
2013-01-17 16:01:33 -0600

    check for unicorn before scheduling compile pages

--- config/initializers/compile_pages.rb
+++ config/initializers/compile_pages.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.application.config.after_initialize do
-    p "args after initialize #{ARGV}"
-    if ARGV.blank?
+    p "command line #{$0}"
+    if $0 =~ /unicorn/
       require './workers/schedule_worker'
       ScheduleWorker.schedule_rebuild_cache
     end
