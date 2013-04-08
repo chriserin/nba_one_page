3efba27f4a44c58ebf8638c60a05c853569b885e
Christopher Erin <chris.erin@gmail.com>
2013-04-08 10:25:57 -0500

    debug

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,6 @@ require File.expand_path('../application', __FILE__)
 #Mongoid.load!("./config/mongoid.yml", Rails.env)
 p "params: #{params}" if defined? params
 p ENV["MONGOHQ_URL"]
+p ENV
 # Initialize the rails application
 NbaOnePage::Application.initialize!
