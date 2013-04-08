6e22a8f732888465f103c394e7b14e62da14161d
Christopher Erin <chris.erin@gmail.com>
2013-04-08 15:35:05 -0500

    add page caching configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ NbaOnePage::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
