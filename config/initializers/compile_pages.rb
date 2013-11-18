6c4d2f95da30eccec9e1ff56c45fafbb16a9b3d8
Christopher Erin <chris.erin@gmail.com>
2013-11-18 13:27:07 -0600

    don't start rebuild cache command

--- config/initializers/compile_pages.rb
+++ config/initializers/compile_pages.rb
@@ -1,9 +1,8 @@
 if Rails.env.production?
   Rails.application.config.after_initialize do
-    p "command line #{$0}"
     if $0 =~ /unicorn/
-      require './workers/schedule_worker'
-      ScheduleWorker.schedule_rebuild_cache
+      ##require './workers/schedule_worker'
+      ##ScheduleWorker.schedule_rebuild_cache
     end
   end
 end
